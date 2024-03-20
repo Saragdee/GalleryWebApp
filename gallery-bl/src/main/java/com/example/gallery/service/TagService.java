ced9fbdd4746ab5512bf9ddcce805b7fc98ca906
Edgaras Mackevičius <edgaras.mackevicius@insoft.lt>
2024-03-20 08:07:00 +0200

    feat: Implement new upload page style and refactor code
    A new style for the upload page has been introduced, providing appealing looks and enhancing user experience. Additionally, code has been refactored in TagService and made more concise. Also made some modifications in masterTemplate.zul, explore.zul and index.zul to facilitate these changes.

--- gallery-bl/src/main/java/com/example/gallery/service/TagService.java
+++ gallery-bl/src/main/java/com/example/gallery/service/TagService.java
@@ -3,26 +3,16 @@ package com.example.gallery.service;
 import com.example.gallery.DTO.TagDto;
 import com.example.gallery.entities.TagEntity;
 import com.example.gallery.repository.TagRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
-import java.util.Set;
-import java.util.stream.Collectors;
-
+@RequiredArgsConstructor
 @Service
 public class TagService {
     private final TagRepository tagRepository;
-
-    public TagService(TagRepository tagRepository) {
-        this.tagRepository = tagRepository;
-    }
 
     public TagEntity convertToEntity(TagDto dto) {
         return tagRepository.findByName(dto.getName()).orElseGet(() -> new TagEntity(dto.getName()));
     }
 
-    public Set<TagEntity> getTags(Collection<TagDto> crap) {
-        return crap.stream().map(this::convertToEntity).collect(Collectors.toSet());
-
-    }
 }
